4400612fec66f2413cd6a7afaf8b2a9802220435
Nathan Bean <natebean@gmail.com>
2019-09-30 19:47:09 -0500

    basic testing started!!

--- src/main/java/com/natebean/GapProductionLogSplitStream.java
+++ src/main/java/com/natebean/GapProductionLogSplitStream.java
@@ -1,7 +1,6 @@
 package com.natebean;
 
 import java.util.Properties;
-import java.util.concurrent.CountDownLatch;
 
 import com.natebean.models.GapLog;
 import com.natebean.models.GapLogProductionLogSplitRecord;
@@ -10,6 +9,7 @@ import com.natebean.models.ProductionLog;
 import com.natebean.producers.GapLogProducer;
 import com.natebean.producers.ProductionLogProducer;
 import com.natebean.transformers.GapProductionLogSplitTransformer;
+import com.natebean.utils.StreamHelpers;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
@@ -23,6 +23,8 @@ import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.state.KeyValueStore;
 
+import io.confluent.common.utils.TestUtils;
+
 public final class GapProductionLogSplitStream {
 
     static final String STREAM_OUTPUT = "gap-production-log-split-json";
@@ -30,57 +32,47 @@ public final class GapProductionLogSplitStream {
 
     public static void main(final String[] args) {
 
-        String broker = "localhost:9092";
-
-        if (args.length > 0) {
-            broker = args[0];
-            System.out.println("Broker: " + args[0]);
-        } else {
-            System.out.println("Default Broker: " + broker);
-        }
-        ;
-
-        final Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "gap-production-log-split-stream-global");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, broker);
-        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        final String broker = StreamHelpers.parseBroker(args);
 
         final StreamsBuilder builder = new StreamsBuilder();
+        createStream(builder);
+        final KafkaStreams streams = new KafkaStreams(builder.build(), getStreamsConfiguration(broker));
+        StreamHelpers.startStream(streams);
+
+    }
+
+    public static void createStream(StreamsBuilder builder) {
 
         builder.globalTable(ProductionLogProducer.SIMPLE_JSON_TOPIC,
                 Materialized.<String, ProductionLog, KeyValueStore<Bytes, byte[]>>as(STATE_STORE_NAME));
 
         KStream<String, GapLog> gapLogStream = builder.stream(GapLogProducer.SIMPLE_JSON_TOPIC,
-                Consumed.with(Serdes.String(), new JSONSerde<>()));
+                Consumed.with(Serdes.String(), new JSONSerde<GapLog>()));
 
-        gapLogStream.flatTransformValues(() -> new GapProductionLogSplitTransformer()).to(STREAM_OUTPUT,
+        gapLogStream.flatTransformValues(GapProductionLogSplitTransformer::new).to(STREAM_OUTPUT,
                 Produced.with(Serdes.String(), new JSONSerde<GapLogProductionLogSplitRecord>()));
 
-        final KafkaStreams streams = new KafkaStreams(builder.build(), props);
-        final CountDownLatch latch = new CountDownLatch(1);
+    }
 
-        // attach shutdown handler to catch control-c
-        Runtime.getRuntime().addShutdownHook(new Thread("streams-threads") {
-            @Override
-            public void run() {
-                System.out.println("closing stream");
-                streams.close();
-                latch.countDown();
-            }
-        });
+    public static Properties getStreamsConfiguration(String bootstrapServers) {
+        final Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "gap-production-log-split-stream-global");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        try {
-            System.out.println("Starting");
-            streams.start();
-            latch.await();
-        } catch (final Throwable e) {
-            e.printStackTrace();
-            System.exit(1);
+        return props;
+    }
+
+    public static Properties getStreamsConfiguration(String bootstrapServers, boolean useTempLocation) {
+        // Use a temporary directory for storing state, which will be automatically
+        // removed after the test.
+        Properties props = getStreamsConfiguration(bootstrapServers);
+        if (useTempLocation) {
+            props.put(StreamsConfig.STATE_DIR_CONFIG, TestUtils.tempDirectory().getAbsolutePath());
         }
-        System.exit(0);
+        return props;
     }
 }
--- src/main/java/com/natebean/models/JSONSerdeCompatible.java
+++ src/main/java/com/natebean/models/JSONSerdeCompatible.java
@@ -6,7 +6,8 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @SuppressWarnings("DefaultAnnotationParam") // being explicit for the example
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "_t")
 @JsonSubTypes({ @JsonSubTypes.Type(value = ProductionLog.class, name = "pl"),
-        @JsonSubTypes.Type(value = GapLog.class, name = "gl") })
+                @JsonSubTypes.Type(value = GapLog.class, name = "gl"),
+                @JsonSubTypes.Type(value = GapLogProductionLogSplitRecord.class, name = "glpl") })
 public interface JSONSerdeCompatible {
 
 }
