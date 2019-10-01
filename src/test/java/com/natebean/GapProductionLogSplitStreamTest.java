a8dcc2054a9ddab64cd18f50dad287095c6df6af
Nathan Bean <natebean@gmail.com>
2019-09-30 20:18:53 -0500

    key builder for gap and production log

--- src/test/java/com/natebean/GapProductionLogSplitStreamTest.java
+++ src/test/java/com/natebean/GapProductionLogSplitStreamTest.java
@@ -18,6 +18,7 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.TopologyTestDriver;
 import org.apache.kafka.streams.state.KeyValueStore;
 import org.apache.kafka.streams.test.ConsumerRecordFactory;
+import org.apache.kafka.streams.test.OutputVerifier;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -79,12 +80,17 @@ public class GapProductionLogSplitStreamTest {
     @Test
     public void testReadOutput() {
 
-        //TODO build key builder
-        //TODO parent class for gap and production log ?
+        ProductionLog pl = new ProductionLog(70, 1, 1, 1L, 50L);
+        GapLog gl = new GapLog(70, 1, 10, 1L, 3L);
+
+        System.out.println(pl.getKafkaKey());
+        System.out.println(gl.getKafkaKey());
+
+        // TODO parent class for gap and production log ?
         ConsumerRecord<byte[], byte[]> plRecord = productionLogFactory.create(ProductionLogProducer.SIMPLE_JSON_TOPIC,
-                "70:1:1", new ProductionLog(70, 1, 1, 1L, 50L));
-        ConsumerRecord<byte[], byte[]> glRecord = gapLogFactory.create(GapLogProducer.SIMPLE_JSON_TOPIC, "70:1:10",
-                new GapLog(70, 1, 1, 1L, 3L));
+                pl.getKafkaKey(), pl);
+        ConsumerRecord<byte[], byte[]> glRecord = gapLogFactory.create(GapLogProducer.SIMPLE_JSON_TOPIC,
+                gl.getKafkaKey(), gl);
 
         testDriver.pipeInput(plRecord);
         testDriver.pipeInput(glRecord);
@@ -92,68 +98,54 @@ public class GapProductionLogSplitStreamTest {
         ProducerRecord<String, GapLogProductionLogSplitRecord> producerRecord = testDriver.readOutput(
                 GapProductionLogSplitStream.STREAM_OUTPUT, stringDeserializer,
                 new JSONSerde<GapLogProductionLogSplitRecord>());
-
-        assertThat(producerRecord.key(), is("70:1:10"));
+        
+        GapLogProductionLogSplitRecord glpl = new GapLogProductionLogSplitRecord(gl, pl);
+        
+        assertThat(producerRecord.key(), is(gl.getKafkaKey()));
+        // OutputVerifier.compareValue(producerRecord, glpl);
 
     }
 
 }
 
-
 /*
-
-    private ProducerRecord<String, Long> readOutput() {
-        return testDriver.readOutput(WordCountLambdaExample.outputTopic, stringDeserializer, longDeserializer);
-    }
-
-    private Map<String, Long> getOutputList() {
-        final Map<String, Long> output = new HashMap<>();
-        ProducerRecord<String, Long> outputRow;
-        while ((outputRow = readOutput()) != null) {
-            output.put(outputRow.key(), outputRow.value());
-        }
-        return output;
-    }
-
-    @Test
-    public void testOneWord() {
-        final String nullKey = null;
-        // Feed word "Hello" to inputTopic and no kafka key, timestamp is irrelevant in
-        // this case
-        testDriver.pipeInput(recordFactory.create(WordCountLambdaExample.inputTopic, nullKey, "Hello", 1L));
-        // Read and validate output
-        final ProducerRecord<String, Long> output = readOutput();
-        OutputVerifier.compareKeyValue(output, "hello", 1L);
-        // No more output in topic
-        assertThat(readOutput()).isNull();
-    }
-
-
-    @Test
-    public void shouldCountWords() {
-        final List<String> inputValues = Arrays.asList("Hello Kafka Streams", "All streams lead to Kafka",
-                "Join Kafka Summit", "И теперь пошли русские слова");
-        final Map<String, Long> expectedWordCounts = new HashMap<>();
-        expectedWordCounts.put("hello", 1L);
-        expectedWordCounts.put("all", 1L);
-        expectedWordCounts.put("streams", 2L);
-        expectedWordCounts.put("lead", 1L);
-        expectedWordCounts.put("to", 1L);
-        expectedWordCounts.put("join", 1L);
-        expectedWordCounts.put("kafka", 3L);
-        expectedWordCounts.put("summit", 1L);
-        expectedWordCounts.put("и", 1L);
-        expectedWordCounts.put("теперь", 1L);
-        expectedWordCounts.put("пошли", 1L);
-        expectedWordCounts.put("русские", 1L);
-        expectedWordCounts.put("слова", 1L);
-
-        final List<KeyValue<String, String>> records = inputValues.stream()
-                .map(v -> new KeyValue<String, String>(null, v)).collect(Collectors.toList());
-        testDriver.pipeInput(recordFactory.create(WordCountLambdaExample.inputTopic, records, 1L, 100L));
-
-        final Map<String, Long> actualWordCounts = getOutputList();
-        assertThat(actualWordCounts).containsAllEntriesOf(expectedWordCounts);
-    }
-
+ * 
+ * private ProducerRecord<String, Long> readOutput() { return
+ * testDriver.readOutput(WordCountLambdaExample.outputTopic, stringDeserializer,
+ * longDeserializer); }
+ * 
+ * private Map<String, Long> getOutputList() { final Map<String, Long> output =
+ * new HashMap<>(); ProducerRecord<String, Long> outputRow; while ((outputRow =
+ * readOutput()) != null) { output.put(outputRow.key(), outputRow.value()); }
+ * return output; }
+ * 
+ * @Test public void testOneWord() { final String nullKey = null; // Feed word
+ * "Hello" to inputTopic and no kafka key, timestamp is irrelevant in // this
+ * case
+ * testDriver.pipeInput(recordFactory.create(WordCountLambdaExample.inputTopic,
+ * nullKey, "Hello", 1L)); // Read and validate output final
+ * ProducerRecord<String, Long> output = readOutput();
+ * OutputVerifier.compareKeyValue(output, "hello", 1L); // No more output in
+ * topic assertThat(readOutput()).isNull(); }
+ * 
+ * 
+ * @Test public void shouldCountWords() { final List<String> inputValues =
+ * Arrays.asList("Hello Kafka Streams", "All streams lead to Kafka",
+ * "Join Kafka Summit", "И теперь пошли русские слова"); final Map<String, Long>
+ * expectedWordCounts = new HashMap<>(); expectedWordCounts.put("hello", 1L);
+ * expectedWordCounts.put("all", 1L); expectedWordCounts.put("streams", 2L);
+ * expectedWordCounts.put("lead", 1L); expectedWordCounts.put("to", 1L);
+ * expectedWordCounts.put("join", 1L); expectedWordCounts.put("kafka", 3L);
+ * expectedWordCounts.put("summit", 1L); expectedWordCounts.put("и", 1L);
+ * expectedWordCounts.put("теперь", 1L); expectedWordCounts.put("пошли", 1L);
+ * expectedWordCounts.put("русские", 1L); expectedWordCounts.put("слова", 1L);
+ * 
+ * final List<KeyValue<String, String>> records = inputValues.stream() .map(v ->
+ * new KeyValue<String, String>(null, v)).collect(Collectors.toList());
+ * testDriver.pipeInput(recordFactory.create(WordCountLambdaExample.inputTopic,
+ * records, 1L, 100L));
+ * 
+ * final Map<String, Long> actualWordCounts = getOutputList();
+ * assertThat(actualWordCounts).containsAllEntriesOf(expectedWordCounts); }
+ * 
  */
