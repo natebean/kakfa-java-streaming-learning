6b8eb4f969e747b8692b32bc86118ba41e7e1af8
G0122NB <nathan.bean@genmills.com>
2019-10-02 14:40:28 -0500

    Struggling with Topology and GlobalTable picking up existing records.

--- src/main/java/com/natebean/TopologyApp.java
+++ src/main/java/com/natebean/TopologyApp.java
@@ -2,9 +2,12 @@ package com.natebean;
 
 import java.util.Properties;
 
+import com.natebean.models.GapLog;
 import com.natebean.models.JSONSerde;
 import com.natebean.models.ProductionLog;
+import com.natebean.processors.PrintProcessor;
 import com.natebean.processors.ProductionLogProcessor;
+import com.natebean.producers.GapLogProducer;
 import com.natebean.producers.ProductionLogProducer;
 import com.natebean.utils.StreamHelpers;
 
@@ -23,15 +26,35 @@ public class TopologyApp {
 
         Topology builder = new Topology();
 
-        StoreBuilder<KeyValueStore<String, ProductionLog>> productionLogStoreSupplier = Stores.keyValueStoreBuilder(
-                Stores.persistentKeyValueStore("productionLogStore"), Serdes.String(), new JSONSerde<ProductionLog>());
+        StoreBuilder<KeyValueStore<String, ProductionLog>> productionLogStoreSupplier = Stores
+                .keyValueStoreBuilder(Stores.persistentKeyValueStore("productionLogStore"), Serdes.String(),
+                        new JSONSerde<ProductionLog>())
+                .withLoggingDisabled();
 
-        builder.addSource("ProductionLogSource", Serdes.String().deserializer(), new JSONSerde<ProductionLog>(),
-                ProductionLogProducer.SIMPLE_JSON_TOPIC)
-        
-                // .addGlobalStore(productionLogStoreSupplier, "ProductionLogSource", Serdes.String(), new JSONSerde<ProductionLog>(), opic, processorName, stateUpdateSupplier)
+        // builder.addSource("ProductionLogSource", Serdes.String().deserializer(), new
+        // JSONSerde<ProductionLog>(),
+        // ProductionLogProducer.SIMPLE_JSON_TOPIC)
 
-                .addProcessor("Process", () -> new ProductionLogProcessor(), "ProductionLogSource");
+        // builder.addGlobalStore(productionLogStoreSupplier,
+        // "ProductionLogStateSource", Serdes.String().deserializer(),
+        // new JSONSerde<ProductionLog>(), ProductionLogProducer.SIMPLE_JSON_TOPIC,
+        // "globalProcessor",
+        // () -> new ProductionLogProcessor("state"))
+
+        // .addProcessor("Process", () -> new ProductionLogProcessor("processor"),
+        // "ProductionLogSource");
+
+        // builder.addGlobalStore(productionLogStoreSupplier, "productionLog", Serdes.String().deserializer(),
+        //         new JSONSerde<ProductionLog>(), ProductionLogProducer.SIMPLE_JSON_TOPIC, "globalProcessor",
+        //         () -> new ProductionLogProcessor("state"))
+
+        //         .addProcessor("Process02", () -> new ProductionLogProcessor("processor02"), "productionLog");
+
+                builder.addSource("gapLogSource", Serdes.String().deserializer(), new JSONSerde<GapLog>(),
+                        GapLogProducer.SIMPLE_JSON_TOPIC)
+
+                .addProcessor("Process03", () -> new PrintProcessor<GapLog>("processor03"), "gapLogSource")
+                ;
 
         // add the count store associated with the WordCountProcessor processor
         // .addStateStore(countStoreBuilder, "Process")
@@ -41,14 +64,15 @@ public class TopologyApp {
         // .addSink("Sink", "sink-topic", "Process");
         final String broker = StreamHelpers.parseBroker(args);
         final KafkaStreams streams = new KafkaStreams(builder, getStreamsConfiguration(broker));
-        streams.cleanUp(); //delete local state
+        // streams.cleanUp(); // delete local state
+        System.out.println(builder.describe());
         StreamHelpers.startStream(streams);
 
     }
 
     public static Properties getStreamsConfiguration(String bootstrapServers) {
         final Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "topology-app-01");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "topology-app-global-test-02");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
--- src/main/java/com/natebean/processors/PrintProcessor.java
+++ src/main/java/com/natebean/processors/PrintProcessor.java
@@ -5,7 +5,7 @@ import com.natebean.models.ProductionLog;
 import org.apache.kafka.streams.processor.Processor;
 import org.apache.kafka.streams.processor.ProcessorContext;
 
-public class ProductionLogProcessor implements Processor<String, ProductionLog> {
+public class PrintProcessor<T> implements Processor<String, T> {
 
     private String prefix;
 
@@ -15,19 +15,19 @@ public class ProductionLogProcessor implements Processor<String, ProductionLog> 
 
     }
 
-    public ProductionLogProcessor(String prefix){
+    public PrintProcessor(String prefix){
         this.prefix = prefix;
-    }
-
-    @Override
-    public void process(String key, ProductionLog value) {
-        System.out.println(prefix + ": " + key);
-
     }
 
     @Override
     public void close() {
         // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void process(String key, T value) {
+        System.out.println(prefix + ": " + key);
 
     }
 
